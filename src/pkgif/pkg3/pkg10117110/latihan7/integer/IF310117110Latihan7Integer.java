ea2baf7daedbeb17b4ce77616b6c2862c1038c45
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:04:33 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan7/integer/IF310117110Latihan7Integer.java
@@ -0,0 +1,31 @@
+
+package pkgif.pkg3.pkg10117110.latihan7.integer;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Bilangan Bulat
+ */
+public class IF310117110Latihan7Integer {
+
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+
+    }
+    
+}
